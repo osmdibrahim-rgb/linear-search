d3005260a2d1e679e916a4f62f90cb4c5dde6477
osmdibrahim-rgb <osmdibrahim@gmail.com>
2026-06-18 10:35:47 +0530

    Add files via upload

---
+++ PROGRAM1.py
@@ -0,0 +1,18 @@
+arr = [7, 12, 18, 25, 30, 45, 50]
+key = int(input("Enter element to search: "))
+
+low = 0
+high = len(arr) - 1
+
+while low <= high:
+    mid = (low + high) // 2
+
+    if arr[mid] == key:
+        print("Element found at index", mid)
+        break
+    elif arr[mid] < key:
+        low = mid + 1
+    else:
+        high = mid - 1
+else:
+    print("Element not found")
